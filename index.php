6cc1d93bc07a4ea5b9b561effbccf1eaf3bd9a6c
Rolmos190 <38666583+Rolmos190@users.noreply.github.com>
2020-06-22 22:53:08 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,20 +4,27 @@
 </head>
 
 <body>
-
+<h1> Our Github Context and Navigation</h1>
     <div class="container">
     	 <section ">
             <!-- link wrapped to allow for image and title to be clicked -->
-            <a >
+            <a>
             <h2 > *Intro* </h2>
             <img class="img_handler" src="">
-            </a> <!-- end link wrap | card information will not be clickable -->
-              <div class="">
-                <h3 class=""><span style="color:black;">Index Page Span</span></h3>
-                <div class="cardbody">Information <br> <br>Information</div>
-    			     <a href=""><button class="">More *</button></a>
-                     <!-- refernce tab btns for centering completely -->
+            </a> 
+            
+            <!-- end link wrap | card information will not be clickable -->
+              
+            <div class="">
+                <h3 class="">
+                <span style="color:black;">Index Page Span</span>
+                </h3>
+
+                <div class="cardbody"> Information <br>
+                <br>Information 
+                </div>
+                <a href=""><button class="">More *</button></a>
+                    <!-- refernce tab btns for centering completely -->
               </div> <!--end imgcard-info-->
           </div> <!-- end imgcard -->
-
-<h1> Our Github Context and Navigation</h1>
+</body>
