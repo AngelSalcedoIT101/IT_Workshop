e800f66280766720fa536694ba49f3ead353e7ed
Rolmos190 <38666583+Rolmos190@users.noreply.github.com>
2020-06-22 22:06:07 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <!DOCTYPE HTML>
 <head>
-	<title>FundraisingATM</title>
+	<title>INDEXs</title>
 </head>
 
 <body>
 
-
     <div class="container">
-    	 <section id="homecards" class="row center-block">
-          <div class="img-card col-xs-12  col-sm-5 col-md-5 col-lg-5" id="img-card-one">
+    	 <section ">
             <!-- link wrapped to allow for image and title to be clicked -->
-            <a class="cardlink" href="welcome.php">
-            <h2 class="card-title text-center">Start a Fundraiser!</h2>
-            <img class="img-responsive cardimage" src="images/Team-Pic.jpg">
+            <a >
+            <h2 > * </h2>
+            <img class="img_handler" src="">
             </a> <!-- end link wrap | card information will not be clickable -->
               <div class="imgcard-info">
                 <h3 class="hvr-underline-from-center text-center"><span style="color:black;">Contact a Rep and get started today!</span></h3>
