7a6a764cc5ec586a515c6ec6045ee05dd4f0c35d
Rolmos190 <38666583+Rolmos190@users.noreply.github.com>
2020-06-22 22:07:38 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <!DOCTYPE HTML>
 <head>
-	<title>INDEXs</title>
+	<title>INDEX</title>
 </head>
 
 <body>
@@ -9,13 +9,13 @@
     	 <section ">
             <!-- link wrapped to allow for image and title to be clicked -->
             <a >
-            <h2 > * </h2>
+            <h2 > *Intro* </h2>
             <img class="img_handler" src="">
             </a> <!-- end link wrap | card information will not be clickable -->
-              <div class="imgcard-info">
-                <h3 class="hvr-underline-from-center text-center"><span style="color:black;">Contact a Rep and get started today!</span></h3>
-                <div class="cardbody">FundraisingATM is here to help you reach your fundraising goals!  Let us set up your free fundraiser and personalized websites for each member.  <br> <br>Everyone is welcome to set up fundraisers for schools, faith-based groups, and various organizations. Fundraising has never been this fun and convenient! FundraisingATM is excited to help you reach your goals and dreams!  Success is just a click away!</div>
-    			     <a href="gettingstarted_sendemail.php"><button class="access btn btn-primary">Learn More!</button></a><!-- refernce tab btns for centering completely -->
+              <div class="">
+                <h3 class=""><span style="color:black;">Index Page Span</span></h3>
+                <div class="cardbody">Information <br> <br>Information</div>
+    			     <a href=""><button class="">More *</button></a><!-- refernce tab btns for centering completely -->
               </div> <!--end imgcard-info-->
           </div> <!-- end imgcard -->
 
