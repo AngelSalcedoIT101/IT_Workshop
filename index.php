fb30086e0dbb61759ac77542730c77735528b168
Rolmos190 <38666583+Rolmos190@users.noreply.github.com>
2020-06-22 22:04:44 -0400

    Update index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE HTML>
+<head>
+	<title>FundraisingATM</title>
+</head>
+
+<body>
+
+
+    <div class="container">
+    	 <section id="homecards" class="row center-block">
+          <div class="img-card col-xs-12  col-sm-5 col-md-5 col-lg-5" id="img-card-one">
+            <!-- link wrapped to allow for image and title to be clicked -->
+            <a class="cardlink" href="welcome.php">
+            <h2 class="card-title text-center">Start a Fundraiser!</h2>
+            <img class="img-responsive cardimage" src="images/Team-Pic.jpg">
+            </a> <!-- end link wrap | card information will not be clickable -->
+              <div class="imgcard-info">
+                <h3 class="hvr-underline-from-center text-center"><span style="color:black;">Contact a Rep and get started today!</span></h3>
+                <div class="cardbody">FundraisingATM is here to help you reach your fundraising goals!  Let us set up your free fundraiser and personalized websites for each member.  <br> <br>Everyone is welcome to set up fundraisers for schools, faith-based groups, and various organizations. Fundraising has never been this fun and convenient! FundraisingATM is excited to help you reach your goals and dreams!  Success is just a click away!</div>
+    			     <a href="gettingstarted_sendemail.php"><button class="access btn btn-primary">Learn More!</button></a><!-- refernce tab btns for centering completely -->
+              </div> <!--end imgcard-info-->
+          </div> <!-- end imgcard -->
+
+<h1> Our Github Context and Navigation</h1>
